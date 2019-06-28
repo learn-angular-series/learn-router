b38dd14e05e1f614202098ae88396ce390d174e2
damoqiongqiu <damoqiongqiu@126.com>
2019-06-28 12:35:03 +0800

    升级到8.0的新写法

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -1,8 +1,9 @@
-import { RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 import {HomeComponent} from './home/home.component';
 import {JokesComponent} from './jokes/jokes.component';
 
-export const appRoutes=[
+export const appRoutes:Routes=[
     {
 		path:'',
 		redirectTo:'home/1',
@@ -21,3 +22,9 @@ export const appRoutes=[
 		component:HomeComponent
 	}
 ];
+
+@NgModule({
+	imports: [RouterModule.forRoot(appRoutes)],
+	exports: [RouterModule]
+})
+export class AppRoutingModule { }
