09f7f4c8dd691c8e56cd620a3a76caabca7d845f
大漠穷秋 <damoqiongqiu@126.com>
2023-03-06 22:17:04 +0800

    refactor-import

--- src/app/common/my-preloading-strategy.ts
+++ src/app/common/my-preloading-strategy.ts
@@ -1,9 +1,8 @@
 import { Route,PreloadingStrategy } from '@angular/router';
-import { Observable } from "rxjs";
-import "rxjs/add/observable/of";
+import { Observable, of } from "rxjs";
 
 export class MyPreloadingStrategy implements PreloadingStrategy {
     preload(route: Route, fn: () => Observable<any>): Observable<any>{
-        return route.data&&route.data.preload?fn():Observable.of(null);
+        return route.data&&route.data.preload?fn():of(null);
     }
 }
